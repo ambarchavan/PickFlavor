b321cb9d022dd625254e980c9af3af6b59023cde
ambar <chavan.amber@siliconstack.com.au>
2019-02-21 12:45:11 +0530

    Initial commit

---
+++ PickFlavor/PickFlavor.h
@@ -0,0 +1,19 @@
+//
+//  PickFlavor.h
+//  PickFlavor
+//
+//  Created by Macbook on 21/02/19.
+//  Copyright © 2019 SiliconStack. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PickFlavor.
+FOUNDATION_EXPORT double PickFlavorVersionNumber;
+
+//! Project version string for PickFlavor.
+FOUNDATION_EXPORT const unsigned char PickFlavorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PickFlavor/PublicHeader.h>
+
+
